c1e55039fc3bca77a242593737a678722dcc7d4c
mphist <mphist@gmail.com>
2024-03-28 23:18:51 -0400

    modify icons

--- plugin/lspsaga.rc.lua
+++ plugin/lspsaga.rc.lua
@@ -7,7 +7,10 @@ saga.setup({
     border = 'rounded',
     colors = {
       normal_bg = '#002b36'
-    }
+    },
+  },
+  lightbulb = {
+    enable = false
   },
 })
 
@@ -17,7 +20,7 @@ vim.keymap.set('n', '<C-j>', '<Cmd>Lspsaga diagnostic_jump_next<CR>', opts)
 vim.keymap.set('n', 'gl', '<Cmd>Lspsaga show_diagnostic<CR>', opts)
 vim.keymap.set('n', 'K', '<Cmd>Lspsaga hover_doc<CR>', opts)
 vim.keymap.set('n', 'gi', '<Cmd>Lspsaga finder<CR>', opts)
--- vim.keymap.set('n', 'gd', '<Cmd>Lspsaga goto_definition<CR>', opts)
+vim.keymap.set('n', 'gd', '<Cmd>Lspsaga goto_definition<CR>', opts)
 -- vim.keymap.set('i', '<C-k>', '<Cmd>Lspsaga signature_help<CR>', opts)
 vim.keymap.set('i', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
 vim.keymap.set('n', 'gp', '<Cmd>Lspsaga peek_definition<CR>', opts)
